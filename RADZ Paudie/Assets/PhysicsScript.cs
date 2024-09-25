7adce984a43bcf4b16d7f13e2c1b60eca9f84f44
STUDENT Paudie G. Fealey <Paudie.G.Fealey@students.ittralee.ie>
2024-09-25 12:52:27 +0100

    Signed-off-by: STUDENT Paudie G. Fealey <Paudie.G.Fealey@students.ittralee.ie>

---
+++ RADZ Paudie/Assets/PhysicsScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsScript : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (rb != null)
+            print("Found Rigid Body");
+        else
+            print("Could Not Find Rigid Body");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.Space))
+            return;
+        rb.AddForce(transform.up);
+    }
+}
